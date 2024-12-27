97e4e5007904c419d467853c11febfe02ace7765
colorful <178195481@qq.com>
2024-12-27 13:56:25 +0000

    vite+svelte基础应用

---
+++ src/MyComponent.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Chen';
+</script>
+
+<main>
+  <h1>Hello I'am {name}!</h1>
+  <input bind:value={name}>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2em;
+  }
+</style>
